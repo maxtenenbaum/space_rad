fb39965b6ee1d06c210dc55dfa32ac49d75f526a
agentmistz <maxtenenbaum223@gmail.com>
2023-12-17 17:09:53 -0800

    Running pipeline and editing functions. added ylim to plot_data keeping ground channels in recording channels for processing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.signal
+import scipy.signal as signal
 from scipy.signal import welch
 import seaborn as sns
 from scipy.signal import butter, filtfilt
@@ -18,7 +18,7 @@ def proper_format(file_path):
 
 
 def mean_ground_correction(data, plot=True):
-    channels = ['PFC', 'hippocampus1', 'hippocampus2', 'striatum1', 'striatum2', 'striatum3', 'ground1', 'ground2', 'ground3']
+    channels = ['PFC', 'hippocampus1', 'hippocampus2', 'striatum1', 'striatum2', 'striatum3','ground1', 'ground2', 'ground3']
     mean_ground = ((data['ground1'] + data['ground2'] + data['ground3'])/3)
     time_vector = np.arange(start=0, stop= (len(data)))
     for chan in channels:
@@ -31,26 +31,42 @@ def mean_ground_correction(data, plot=True):
             ax[i].set_title(chan)
         plt.suptitle('Ground Corrected Data', fontsize=48,y=0.93)
         plt.show()
+    return data
 
-
-def bandpass_of_interest(data, lo, hi, fs=1024, plot=True):
-    recording_channels = ['PFC', 'hippocampus1', 'hippocampus2', 'striatum1', 'striatum2', 'striatum3']
+"""
+def bandpass_of_interest(data, lo, hi, fs=1024, plot=False):
+    channels = ['PFC', 'hippocampus1', 'hippocampus2', 'striatum1', 'striatum2', 'striatum3']
     sos = signal.butter(lo,hi,'lp', fs, output = 'sos')
     time_vector = np.arange(start=0, stop= (len(data)))
     for chan in channels:
         data[chan] = signal.sosfiltfilt(sos, data[chan])
     if plot == True:
-        fig, ax = plt.subplots(nrows=len(recording_channels), ncols=1, figsize=(24,24), sharex=True)
-        for chan in recording_channels:
+        fig, ax = plt.subplots(nrows=len(channels), ncols=1, figsize=(24,24), sharex=True)
+        for chan in channels:
             i = channels.index(chan)
             ax[i].plot(time_vector, data[chan])
             ax[i].set_title(chan, fontsize=24)
         plt.suptitle('Bandpassed Data', fontsize=48,y=0.93)
         plt.show()
     else:
-        return
+        return"""
 
-
+def bandpass_of_interest(data, lo, hi, fs=1024, plot=False):
+    N = 4  # Example order, adjust based on your needs
+    channels = ['PFC', 'hippocampus1', 'hippocampus2', 'striatum1', 'striatum2', 'striatum3', 'ground1', 'ground2', 'ground3']
+    sos = signal.butter(N, (lo, hi), 'bp', fs=fs, output='sos')
+    time_vector = np.arange(start=0, stop=(len(data)))
+    for chan in channels:
+        data[chan] = signal.sosfiltfilt(sos, data[chan])
+    if plot:
+        fig, ax = plt.subplots(nrows=len(channels), ncols=1, figsize=(24, 24), sharex=True)
+        for i, chan in enumerate(channels):
+            ax[i].plot(time_vector, data[chan])
+            ax[i].set_title(chan, fontsize=24)
+        plt.suptitle('Bandpassed Data', fontsize=48, y=0.93)
+        plt.show()
+    else:
+        return data  # Returning data might be more useful than just 'return'
 
 
 
--- tests/hypercap.py
+++ tests/hypercap.py
@@ -14,18 +14,17 @@ rat55_raw = preprocessing.proper_format("/Volumes/Elements/Space Radiation Proje
 rat55_raw_plot = visualization.plot_data(rat55_raw, "Rat55 Raw Data")
 
 # Bandpass raw data
-rat55_theta_raw = preprocessing.bandpass_of_interest(rat55_raw, 4, 8)
-rat55_alpha_raw = preprocessing.bandpass_of_interest(rat55_raw, 8, 13)
-rat55_beta_raw = preprocessing.bandpass_of_interest(rat55_raw, 13, 30)
+rat55_theta = preprocessing.bandpass_of_interest(rat55_raw, 4, 8)
+rat55_alpha = preprocessing.bandpass_of_interest(rat55_raw, 8, 13)
+rat55_beta = preprocessing.bandpass_of_interest(rat55_raw, 13, 30)
 
 # Ground correct raw data
-rat55_theta_corrected = preprocessing.mean_ground_correction(rat55_theta_raw, plot='False')
-rat55_alpha_corrected = preprocessing.mean_ground_correction(rat55_alpha_raw, plot='False')
-rat55_beta_corrected = preprocessing.mean_ground_correction(rat55_beta_raw, plot='False')
+preprocessing.mean_ground_correction(rat55_theta, plot='False')
+preprocessing.mean_ground_correction(rat55_alpha, plot='False')
+preprocessing.mean_ground_correction(rat55_beta, plot='False')
 
 # Plot processed data
-
-rat55_theta_plot = visualization.plot_data(rat55_theta_corrected, "Rat55 Theta Band Data")
-rat55_alpha_plot = visualization.plot_data(rat55_alpha_corrected, "Rat55 Alpha Band Data")
-rat55_beta_plot = visualization.plot_data(rat55_beta_corrected, "Rat55 Beta Band Data")
+rat55_theta_plot = visualization.plot_data(rat55_theta, "Rat55 Theta Band Data")
+rat55_alpha_plot = visualization.plot_data(rat55_alpha, "Rat55 Alpha Band Data")
+rat55_beta_plot = visualization.plot_data(rat55_beta, "Rat55 Beta Band Data")
 
