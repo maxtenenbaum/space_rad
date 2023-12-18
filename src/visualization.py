1ce7d87b7bd942dd264dc009b5a2abd8b0b44a4e
agentmistz <maxtenenbaum223@gmail.com>
2023-12-17 18:29:08 -0800

    New plots

--- src/visualization.py
+++ src/visualization.py
@@ -14,8 +14,60 @@ def plot_data(data, title):
         i = channels.index(chan)
         ax[i].plot(time_vector/1024, data[chan])
         ax[i].set_title(chan, fontsize=24)
-        ax[i].set_ylim(-25,25)
+        #ax[i].set_ylim(-25,25)
     plt.suptitle(f'{title}', fontsize=48,y=0.93)
     plt.savefig(os.path.join(output_dir, f"{title}.png"))  # Ensure the filename is valid
     plt.close()
 
+
+def power_spectral_density(data, fs=1024, title='Data'):
+    output_dir = "output/plots/"
+    os.makedirs(output_dir, exist_ok=True)  # Ensure output directory exists
+
+    recording_channels = ['PFC', 'hippocampus1', 'hippocampus2', 'striatum1', 'striatum2', 'striatum3']
+    plt.figure(figsize=(24, 24))  # Create a figure
+
+    # Define a list of colors, one for each channel
+    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']  # Add more colors if needed
+
+    for i, chan in enumerate(recording_channels):
+        f, S = signal.periodogram(data[chan], fs, scaling='density', nfft=1024)
+        plt.semilogy(f, S, label=chan, color=colors[i % len(colors)], linewidth=4)
+
+    plt.xlabel('Frequency [Hz]', fontsize=36)
+    plt.ylabel('PSD [V**2/Hz]', fontsize=36)
+    plt.xlim(2, 50)
+    plt.tick_params(axis='x', labelsize=24)
+    plt.tick_params(axis='y', labelsize=24)
+    plt.ylim(0.0001,1000)
+    plt.grid(visible=True, which='major')
+    plt.title(title, fontsize=48)
+    plt.legend()  # Add a legend to distinguish channels
+
+    plt.savefig(os.path.join(output_dir, f"{title}.png"))  # Save the figure
+    plt.close()  # Close the figure
+
+def plot_power(data, title, reference_power=1):
+    output_dir = "output/plots/"
+    channels = ['PFC', 'hippocampus1', 'hippocampus2', 'striatum1', 'striatum2', 'striatum3']
+    time_vector = np.arange(len(data)) / 1024  # Assuming a sample rate of 1024 Hz for time axis
+
+    fig, ax = plt.subplots(figsize=(24, 24))
+
+    # Define a list of colors, one for each channel
+    colors = ['b', 'g', 'r', 'c', 'm','k']  # Add more colors if you have more channels
+
+    for i, chan in enumerate(channels):
+        # Calculate power and convert to decibels
+        power = np.square(data[chan])  # Power of the signal
+        decibels = 10 * np.log10(power / reference_power)
+
+        ax.plot(time_vector, decibels, label=chan, color=colors[i % len(colors)])
+
+    ax.set_xlabel("Time (seconds)")
+    ax.set_ylabel("Power (dB)")
+    #ax.set_xlim(500,505)
+    ax.set_title(title)
+    ax.legend()  # Add a legend to distinguish channels
+
+    plt.savefig(os.path.join(output_dir, f"{title}.png"))  # Ensure the filename is valid
